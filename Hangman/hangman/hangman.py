90b472964fd01a59429c410f11fda580670e6022
locn3sz <50171357+locn3sz@users.noreply.github.com>
2021-01-24 16:19:42 -0500

    Initial Commit
    Phase 1/8 of Hangman Game

---
+++ Hangman/hangman/hangman.py
@@ -0,0 +1,15 @@
+'''
+Take on the popular game Hangman
+Eventually will include the following aspects
+    In the main menu, a player can choose to either play or exit the game.
+    If the user chooses to play, the computer picks a word from a list: this will be the answer to the puzzle.
+    The computer asks the player to enter a letter that they think is in the word.
+    If that letter does not appear in the word and this letter hasn't already been guessed, the computer counts it as a miss. A player can only afford 8 misses before the game is over.
+    If the letter does occur in the word, the computer notifies the player. If there are letters left to guess, the computer invites the player to go on.
+    When the entire word is uncovered, it's a victory! The game calculates the final score and returns to the main menu.
+
+Going to begin the initial build out by announcing to the user via console the current game status
+'''
+
+print("H A N G M A N")
+print("The game will be available soon.")
