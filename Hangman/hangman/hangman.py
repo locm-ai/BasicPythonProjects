1665640626c081a86cc567024883005635be39f2
locm.ai <loc.d.mai@outlook.com>
2021-01-28 11:44:45 -0500

    Sixth Commit
    Added input validation as well as removal of attempts should a user provide an invalid input

--- Hangman/hangman/hangman.py
+++ Hangman/hangman/hangman.py
@@ -1,5 +1,5 @@
 # Write your code here
-import random
+import random  # Allows us to randomize starting word
 '''
 Take on the popular game Hangman
 Eventually will include the following aspects
@@ -11,9 +11,11 @@ Eventually will include the following aspects
     When the entire word is uncovered, it's a victory! The game calculates the final score and returns to the main menu.
 
 Adding the following functionality
-    Print That letter does not appear in the word and reduce the number of remaining attempts if the word selected by the program doesn't contain this letter.
-    Print No improvements and reduce the attempts count if the selected word contains this letter but the user has already tried guessing it.
-    The number of remaining attempts should be decreased only if there are no letters to uncover.
+    If the user enters the same letter twice, then the program should output You've already guessed this letter . This message should also be printed if the user inputs a letter that doesn't appear in the word. The number of attempts shouldn't be decreased in this case.
+    Also, you should check to make sure the player entered an English lowercase letter. If not, the program should print Please enter a lowercase English letter .
+    You should also check if the player entered exactly one letter. If not, the program should print You should input a single letter . Remember that zero is also not one!
+    Note that none of these three errors should reduce the number of remaining attempts!
+
 
 '''
 
@@ -27,22 +29,25 @@ while remaining_attempts > 0:  # Looping structure until user is out of attempts
     print()
     print(f"{word_display}")  # display the word, hidden by "-"
     guess = input("Input a letter: ")  # Prompts user for a guess(with hint provided) and stores as a string
-    if guess in hidden_word and guess in guessed_letters:
-        print("No improvements")
-        remaining_attempts -= 1  # lowers remaining attempts
-    elif guess not in hidden_word:  # checks if the users guess is a part of the hidden_word and outputs message if it is not
-        print("That letter doesn't appear in the word")
-        remaining_attempts -= 1  # lowers remaining attempts
-    guessed_letters.append(guess)  # adds guess to list of guessed_letters
+    guessed_letters.append(guess)  # Adds the users guess to guessed_letters along side the users previous attempts
     temp_display = hidden_word  # copy of hidden_word
     for letters in hidden_word:  # parses through every character in hidden_word
          if letters not in guessed_letters:  # boolean check if a character in hidden_word is a part of guessed_letters
              temp_display = temp_display.replace(letters, "-")  #replaces every character that fulfils the above requirements with "-"
     word_display = temp_display  # overwrite what is display at the start of the loop
-    if word_display == hidden_word:
+    if word_display == hidden_word:  # Checks if the user has solved the word, If so break out of loop
         break
-    # print("\n")  # new line for formatting
-if word_display == hidden_word:
+    if len(guess) > 1:  # Checks if the input is multiple characters
+        print("You should input a single letter")
+    elif guessed_letters.count(guess) > 1:  # Checks if the input is in the previous guesses (current guess already appended to list)
+        print("You've already guessed this letter")
+    elif (not guess.isalpha() or guess.isupper()):  # Checks if the input is a valid character and in lower case format
+        print("Please enter a lowercase English letter")
+    elif guess not in hidden_word:  # Checks if the input is a part of the hidden word
+        print("That letter doesn't appear in the word")
+        remaining_attempts = remaining_attempts - 1  # Reduces remaining attempts
+
+if word_display == hidden_word:  # Win/Loss conditions as well as the resulting outputs
     print(word_display)
     print("You guessed the word!")
     print("You survived!")
