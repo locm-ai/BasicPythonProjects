25dfd700890877944b92c36d1ca49393a1d26eef
locm.ai <loc.d.mai@outlook.com>
2021-02-01 23:17:31 -0500

    Initial Commit
    Virtual coffee machine

---
+++ CoffeeMachine/machine/coffee_machine.py
@@ -0,0 +1,18 @@
+"""
+Starting with a program that makes you a coffee – virtual coffee, of course.
+In this project, you will implement functionality that simulates a real coffee
+machine. It can run out of ingredients, such as milk or coffee beans, it can
+offer you various types of coffee, and, finally, it will take money for the
+prepared drink.
+
+The first version of the program just makes you a coffee. It should print to
+the standard output what it is doing as it makes the drink.
+"""
+
+print("Starting to make a coffee")
+print("Grinding coffee beans")
+print("Boiling water")
+print("Mixing boiled water with crushed coffee beans")
+print("Pouring coffee into the cup")
+print("Pouring some milk into the cup")
+print("Coffee is ready!")
