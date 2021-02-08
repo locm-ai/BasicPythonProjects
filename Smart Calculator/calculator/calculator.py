4e1339719cbce4833fb53ea6fde85ddc0d2a06c7
locm.ai <loc.d.mai@outlook.com>
2021-02-08 12:23:13 -0500

    Update calculator.py
    Added a initial loop for repeating user input and a exit condition.  Able to handle single, multiple, empty inputs

--- Smart Calculator/calculator/calculator.py
+++ Smart Calculator/calculator/calculator.py
@@ -1,21 +1,29 @@
 """
-    Our task is to make a smart calculator. What do you usually expect from a
-calculator? Of course, it is bound to support 4 basic operations:
-multiplication, division, addition, and subtraction. But we don't need to stop
-there and add the ability to calculate expressions containing parenthesis and
-the ability to remember the previous result. This will be a console
-application, which we will improve gradually, from simple to complex.
-
-    So, the first version of the calculator will only support the addition
-operation for two numbers. In this version, we will not receive the plus
-symbol (+) as an input, initially we will accept this as an unwritten rule
-(because only one operation is supported).
+    It is time to improve the previous version of the calculator. What if we
+have many pairs of numbers, the sum of which we need to find? It will be very
+inconvenient to run the program every time. So then let's add a loop to
+continuously calculate the sum of two numbers. We will also be sure to have a
+safeword to break the loop. Also, It would be nice to think through situations
+where users enter only one number or do not enter numbers at all.
 
 Current Version
-Write a program that reads two integer numbers from the same line and prints
-their sum in the standard output. Numbers can be positive, negative, or zero.
+    Write a program that reads two numbers in a loop and prints the sum in the
+standard output.
+    If a user enters only a single number, the program should print the same
+number. If a user enters an empty line, the program should ignore it.
+    When the command /exit is entered, the program must print "Bye!" (without
+quotes), and then stop.
 """
-
 user_input = input()
-numbers = user_input.split(' ')
-print(int(numbers[0]) + int(numbers[1]))
+while user_input != "/exit":
+    if user_input == "":
+        user_input = input()
+    numbers = [int(x) for x in user_input.split(' ')]
+    if len(numbers) > 1:
+        print(numbers[0] + numbers[1])
+        user_input = input()
+    elif len(numbers) == 1:
+        print(int(user_input))
+        user_input = input()
+
+print("Bye!")
