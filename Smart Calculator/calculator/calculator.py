d1768972bdf1f37d33d4634a773233315f370cfd
locm.ai <loc.d.mai@outlook.com>
2021-02-12 17:26:08 -0500

    Update calculator.py
    Removed some redundant code. Evaluates expressions.

--- Smart Calculator/calculator/calculator.py
+++ Smart Calculator/calculator/calculator.py
@@ -1,29 +1,31 @@
 """
-    It is time to improve the previous version of the calculator. What if we
-have many pairs of numbers, the sum of which we need to find? It will be very
-inconvenient to run the program every time. So then let's add a loop to
-continuously calculate the sum of two numbers. We will also be sure to have a
-safeword to break the loop. Also, It would be nice to think through situations
-where users enter only one number or do not enter numbers at all.
+    Finally, we got to the next operation: subtraction. It means that from
+now on the program must receive the addition + and subtraction - operators as
+an input to distinguish operations from each other. It must support both unary
+and binary minus operators. Moreover, If the user has entered several same
+operators following each other, the program still should work (like Java or
+Python REPL). Also, as you remember from school math, two adjacent minus signs
+turn into a plus. The smart calculator ought to have such a feature.
 
-Current Version
-    Write a program that reads two numbers in a loop and prints the sum in the
-standard output.
-    If a user enters only a single number, the program should print the same
-number. If a user enters an empty line, the program should ignore it.
-    When the command /exit is entered, the program must print "Bye!" (without
-quotes), and then stop.
+    The program must calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4,
+and so on.
+    Modify the result of the /help command to explain these operations.
+    Decompose your program using functions to make it easy to understand and
+edit later.
+    The program should not stop until the user enters the /exit command.
+    If you encounter an empty line, do not output anything.
 """
 user_input = input()
 while user_input != "/exit":
     if user_input == "":
-        user_input = input()
-    numbers = [int(x) for x in user_input.split(' ')]
-    if len(numbers) > 1:
-        print(numbers[0] + numbers[1])
-        user_input = input()
-    elif len(numbers) == 1:
-        print(int(user_input))
-        user_input = input()
-
+        pass
+    elif user_input == "/help":
+        print("The program calculates the sum of numbers")
+    else:
+        instructions = user_input.split()
+        if len(user_input) > 1:
+            print(eval(user_input))
+        elif len(user_input) == 1:
+            print(user_input[0])
+    user_input = input()
 print("Bye!")
